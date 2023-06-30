03016b5bfcd433dc4851423042f05f06fca523ae
aymenzahmoul <98813612+aymenzahmoul@users.noreply.github.com>
2023-06-30 15:15:35 +0100

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "mail@tharindu.dev",
+		logo: "../svsvs.jpg",
+	},
+
+	socials: {
+		
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile ",
+		description:
+			"Je suis un développeur front-end, je suis responsable de la création d'interfaces utilisateur d'applications et de sites Web. Je maîtrise les principaux langages comme HTML, CSS et JavaScript et j'utilise des frameworks populaires comme React, Angular ou Vue.js. Je travaille avec des concepteurs et des développeurs backend pour transformer des prototypes en code fonctionnel, garantissant la réactivité de l'interface, la compatibilité du navigateur et des performances optimisées. Je porte également une attention particulière à l'accessibilité et à l'utilisation d'outils pour créer des interfaces utilisateur accessibles. J'utilise des outils de développement tels que des testeurs de navigateur, des gestionnaires de packages et des bundlers, et j'effectue des tests pour garantir la qualité du code. Je suis constamment à la recherche de tendances et de développements dans le domaine du développement d'interfaces utilisateur, essayant toujours de me mettre à jour pour trouver les meilleures solutions.",
+	},
+
+	about: {
+		title: "I’m Aymen Zahmoul. I live in Tunis djerba",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
